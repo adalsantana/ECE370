14921f2309a577ca98c36212078b7d9fa379b7f8
masterbum <asantan6@protonmail.com>
2019-05-14 15:57:48 -0400

    set up barebones script to receive udp

--- UDP/udp_recv.py
+++ UDP/udp_recv.py
@@ -1,5 +1,14 @@
 import socket
+import udp_struct
 
-UDP_IP = "192.168.1.1"
+UDP_IP = "127.0.0.1" #placeholder
 UDP_PORT = 4242
 
+recv = udp_struct.receive()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) #buffer size 1024 bytes
+    print "received message: "
