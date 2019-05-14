372691aea79adc63c9f6379cfd771336ead17ef8
masterbum <asantan6@protonmail.com>
2019-05-14 14:24:31 -0400

    header file containing struct and AP password

---
+++ CompassOdometry/secrets.h
@@ -0,0 +1,15 @@
+#define SECRET_SSID "feather"
+#define SECRET_PASS "pass"
+#define UDP_PORT_LISTEN 2363
+
+typedef struct udp_recv{
+  double velocity; 
+  double theta; 
+  int rst; 
+}__attribute__((packed)) udp_recv_t; 
+
+typedef struct udp_send{
+  double imu[6]; 
+  double odo[3]; 
+  double heading; 
+}__attribute__((packed)) udp_send_t; 
