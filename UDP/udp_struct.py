6a29ee0207bcdbb4587e12030e7588b4f3bf3586
masterbum <asantan6@protonmail.com>
2019-05-14 15:21:16 -0400

    added struct to hold imu data

--- UDP/udp_struct.py
+++ UDP/udp_struct.py
@@ -1,8 +1,18 @@
-from ctypes import Structure, c_double, c_uint8
+from ctypes import *
 
 class message(Structure):
 	_pack_=1
 	_fields_ = [	("velocity", c_double), 
 			("theta", c_double), 
                         ("rst", c_uint8)
+                    ]
+
+class imu(Structure):
+        _pack_ = 1
+        _fields_ = [    ("ax", c_double), 
+                        ("ay", c_double), 
+                        ("az", c_double).
+                        ("mx", c_double), 
+                        ("my", c_double), 
+                        ("mz", c_double)
                     ]
