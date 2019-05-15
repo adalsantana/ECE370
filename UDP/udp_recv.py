5a37b03600988b7131032e9df718e29fb65f7bc2
masterbum <asantan6@protonmail.com>
2019-05-14 20:39:01 -0400

    current state allows for successful printing of data collected by IMU

--- UDP/udp_recv.py
+++ UDP/udp_recv.py
@@ -2,8 +2,8 @@ import socket
 import udp_struct
 
 #UDP_IP = "192.168.4.26"
-UDP_IP = "127.0.0.1" #placeholder
-#UDP_IP = "192.168.1.100"
+#UDP_IP = "127.0.0.1" #placeholder
+UDP_IP = "192.168.1.100"
 UDP_PORT = 4242
 
 recv = udp_struct.receive()
@@ -17,20 +17,20 @@ def udpToStruct(data):
     #make it the size of received data
     ba.extend(data)
     #put it in receive udp structure
-    ret = udp_struct.receive().from_buffer(ba)
+    udpStr = udp_struct.receive.from_buffer(ba)
+    return udpStr
 
 while True:
     data, addr = sock.recvfrom(1024) #buffer size 1024 bytes
     print "received message\n"
     recv = udpToStruct(data)
-    
-    print "imu ax, ay, az, mx, my, mz"
-    for i in range(len(recv.ImuData)):
-        print recv.ImuData[i]
-    print "IMU header"
-    print recv.heading
-    
-    print "odometry x, y, z"
-    for i in range(len(recv.OdometryData)):
-        print recv.OdometryData[i]
-    
+    print "imu ax " + str(recv.ImuData.ax)
+    print "imu ay " + str(recv.ImuData.ay)
+    print "imu az " + str(recv.ImuData.az)
+    print "imu mx " + str(recv.ImuData.mx)
+    print "imu my " + str(recv.ImuData.my)
+    print "imu mz " + str(recv.ImuData.mz)
+    print "Odometry x location " + str(recv.OdometryData.x)
+    print "Odometry y location " + str(recv.OdometryData.y)
+    print "Odometry z location " + str(recv.OdometryData.z)
+    print "IMU Heading " + str(recv.heading)
