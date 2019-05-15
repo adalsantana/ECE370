5c620200107e6785378d76e43b549f4043a0027f
masterbum <asantan6@protonmail.com>
2019-05-14 21:54:26 -0400

    previously submitted version of final code

---
+++ FinalCode/FinalCode/secrets.h
@@ -0,0 +1,39 @@
+#define SECRET_SSID "feather"
+#define SECRET_PASS "pass"
+#define UDP_PORT_LISTEN 2363
+#define UDP_PORT_SEND 4242
+#define returnRate 0.5 //Hz
+
+#define DESTINATION_OCT_1 192
+#define DESTINATION_OCT_2 168
+#define DESTINATION_OCT_3 1
+#define DESTINATION_OCT_4 100
+
+#define NETMASK_1 255
+#define NETMASK_2 255
+#define NETMASK_3 255
+#define NETMASK_4 0
+
+#define NETWORK_IP_1 192
+#define NETWORK_IP_2 168
+#define NETWORK_IP_3 1
+#define NETWORK_IP_4 1
+
+
+typedef struct udp_recv{
+  double velocity; 
+  double theta; 
+  int rst; 
+}__attribute__((packed)) udp_recv_t; 
+
+typedef struct udp_send{
+  double imu[6]; 
+  double odo[3]; 
+  double heading; 
+}__attribute__((packed)) udp_send_t; 
+
+typedef struct values{
+  double velocity; 
+  double theta; 
+  int rst; 
+};
